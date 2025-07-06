d8e1d178dd6d32692593e313ab6a263f55fa8b22
yasin <yasingoorakani@gmail.com>
2025-07-06 19:34:08 +0330

    make gitignore more flexiable

---
+++ services/auth/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/server.ts',
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    target: 'node',
+};
